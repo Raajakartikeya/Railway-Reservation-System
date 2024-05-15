fa563cf14a53edd767fc7652d8b0275cd7d05015
Raaja Kartikeya <raaja@Raajas-MacBook-Air.local>
2024-05-15 20:06:01 +0530

    3rd

--- main.cpp
+++ main.cpp
@@ -26,7 +26,8 @@ public:
         this->age = age;
         this->gender = gender;
         this->berthPreferance = berthPreferance;
-        this->passengerId = ++id;
+        this->passengerId = id;
+        id = id + 1;
     }
 };
 int Passenger::id = 1;
@@ -45,9 +46,8 @@ public:
     vector<int> racPositions;
     vector<int> waitingListPositions;
     map<int, Passenger> confirmedTickets;
-    queue <int> racPassengers;
-    queue <int> waitingListPassengers;
-    
+    queue<int> racPassengers;
+    queue<int> waitingListPassengers;
 
     Ticket()
     {
@@ -64,64 +64,67 @@ public:
     }
     void printBookedTickets()
     {
-        cout<<setw(25)<<"Passenger ID"<<setw(25)<<"Name"<<setw(25)<<"Age"<<setw(30)<<"Gender"<<setw(30)<<"Seat Alloted"<<endl;
-        cout<<"------------------------------------------------------------------------------------------------------------------------------------------------\n";
-        for(auto it = confirmedTickets.begin(); it != confirmedTickets.end(); it++)
+        cout << setw(25) << "Passenger ID" << setw(25) << "Name" << setw(25) << "Age" << setw(30) << "Gender" << setw(30) << "Seat Alloted" << endl;
+        cout << "------------------------------------------------------------------------------------------------------------------------------------------------\n";
+        for (auto it = confirmedTickets.begin(); it != confirmedTickets.end(); it++)
         {
-           cout<<setw(25)<<it->second.passengerId<<setw(25)<<it->second.name<<setw(25)<<it->second.age<<setw(30)<<it->second.gender<<setw(24)<<it->second.seatNumber<<" "<<it->second.alloted<<endl;
-           cout<<"------------------------------------------------------------------------------------------------------------------------------------------------\n";
+            cout << setw(25) << it->second.passengerId << setw(25) << it->second.name << setw(25) << it->second.age << setw(30) << it->second.gender << setw(24) << it->second.seatNumber << " " << it->second.alloted << endl;
+            cout << "------------------------------------------------------------------------------------------------------------------------------------------------\n";
         }
     }
     void viewAvailableTickets()
     {
         int totalTickets = lowerBerths + upperBerths + middleBerths;
-        if(totalTickets > 0)
+        if (totalTickets > 0)
         {
-            cout<<endl<<"Available Tickets: " <<totalTickets<<" Sleeper Berths"<<endl;
+            cout << endl
+                 << "Available Tickets: " << totalTickets << " Sleeper Berths" << endl;
         }
-        else if(rac > 0)
+        else if (rac > 0)
         {
-            cout<<"\nNo Sleeper Berths Available\n";
-            cout<<"\nTotal RACs Available:"<<rac<<"\n";
+            cout << "\nNo Sleeper Berths Available\n";
+            cout << "\nTotal RACs Available:" << rac << "\n";
         }
-        else if(waitingList > 0){
-            cout<<"\nNo Sleeper Berths and RACs Available\n";
-            cout<<"\nWaiting List:"<<waitingList<<"\n";
+        else if (waitingList > 0)
+        {
+            cout << "\nNo Sleeper Berths and RACs Available\n";
+            cout << "\nWaiting List:" << waitingList << "\n";
         }
-        else{
-            cout<<"\nNo Tickets Available\n";
+        else
+        {
+            cout << "\nNo Tickets Available\n";
         }
     }
     void cancelTicket()
     {
         int cId;
-        cout<<"Enter Passenger ID to be cancelled:";
-        cin>>cId;
-        for(auto it = confirmedTickets.begin(); it!=confirmedTickets.end(); it++)
+        cout << "Enter Passenger ID to be cancelled:";
+        cin >> cId;
+        for (auto it = confirmedTickets.begin(); it != confirmedTickets.end(); it++)
         {
-            if(it->first == cId)
+            if (it->first == cId)
             {
-                if(it->second.alloted == "lower")
+                if (it->second.alloted == "lower")
                 {
                     lowerBerths++;
                     lowerPositions.push_back(it->second.seatNumber);
                 }
-                else if(it->second.alloted == "middle")
+                else if (it->second.alloted == "middle")
                 {
                     middleBerths++;
                     middlePositions.push_back(it->second.seatNumber);
                 }
-                else if(it->second.alloted == "upper")
+                else if (it->second.alloted == "upper")
                 {
                     upperBerths++;
                     upperPositions.push_back(it->second.seatNumber);
                 }
-                else if(it->second.alloted == "RAC")
+                else if (it->second.alloted == "RAC")
                 {
                     rac++;
                     racPositions.push_back(it->second.seatNumber);
                 }
-                else if(it->second.alloted == "Waiting List")
+                else if (it->second.alloted == "Waiting List")
                 {
                     waitingList++;
                     waitingListPositions.push_back(it->second.seatNumber);
@@ -130,55 +133,61 @@ public:
                 break;
             }
         }
-        if(racPassengers.size() > 0 && (lowerBerths > 0 || upperBerths > 0 || middleBerths > 0))
+        if (racPassengers.size() > 0 && (lowerBerths > 0 || upperBerths > 0 || middleBerths > 0))
         {
             int id = racPassengers.front();
             Passenger p = confirmedTickets[id];
-            int racSeat = p.seatNumber; 
+            int racSeat = p.seatNumber;
             if ((p.berthPreferance == "lower" && lowerBerths > 0) || (p.berthPreferance == "middle" && middleBerths > 0) || (p.berthPreferance == "upper" && upperBerths > 0))
             {
-                if(lowerBerths > 0 && p.berthPreferance == "lower")
+                if (lowerBerths > 0 && p.berthPreferance == "lower")
                 {
                     confirmedTickets[id].alloted = "lower";
                     confirmedTickets[id].seatNumber = lowerPositions[0];
                     lowerPositions.erase(lowerPositions.begin());
+                    lowerBerths--;
                 }
-                else if(middleBerths > 0 && p.berthPreferance == "middle")
+                else if (middleBerths > 0 && p.berthPreferance == "middle")
                 {
                     confirmedTickets[id].alloted = "middle";
                     confirmedTickets[id].seatNumber = middlePositions[0];
                     middlePositions.erase(middlePositions.begin());
+                    middleBerths--;
                 }
                 else
                 {
                     confirmedTickets[id].alloted = "upper";
                     confirmedTickets[id].seatNumber = upperPositions[0];
                     upperPositions.erase(upperPositions.begin());
+                    upperBerths--;
                 }
             }
-            else if(lowerBerths > 0)
+            else if (lowerBerths > 0)
             {
                 confirmedTickets[id].alloted = "lower";
                 confirmedTickets[id].seatNumber = lowerPositions[0];
                 lowerPositions.erase(lowerPositions.begin());
+                lowerBerths--;
             }
-            else if(middleBerths > 0)
+            else if (middleBerths > 0)
             {
                 confirmedTickets[id].alloted = "middle";
                 confirmedTickets[id].seatNumber = middlePositions[0];
                 middlePositions.erase(middlePositions.begin());
+                middleBerths--;
             }
-            else if(upperBerths > 0)
+            else if (upperBerths > 0)
             {
                 confirmedTickets[id].alloted = "upper";
                 confirmedTickets[id].seatNumber = upperPositions[0];
                 upperPositions.erase(upperPositions.begin());
+                upperBerths--;
             }
             racPassengers.pop();
             rac++;
             racPositions.push_back(racSeat);
         }
-        if(waitingListPassengers.size() > 0 && rac > 0)
+        if (waitingListPassengers.size() > 0 && rac > 0)
         {
             int id = waitingListPassengers.front();
             Passenger p = confirmedTickets[id];
@@ -186,6 +195,7 @@ public:
             confirmedTickets[id].alloted = "RAC";
             confirmedTickets[id].seatNumber = racPositions[0];
             racPositions.erase(racPositions.begin());
+            racPassengers.push(id);
             waitingListPassengers.pop();
             waitingList++;
             rac--;
@@ -205,36 +215,36 @@ public:
             cout << "\nPreferred Berth Available";
             if (p.berthPreferance == "lower")
             {
-                
+
                 p.alloted = p.berthPreferance;
                 ticket.lowerBerths--;
                 p.seatNumber = ticket.lowerPositions[0];
                 ticket.lowerPositions.erase(ticket.lowerPositions.begin());
                 ticket.confirmedTickets[p.passengerId] = p;
-                cout << "\nBooked Succesfully with seat number "<<p.seatNumber<<" "<<p.alloted<<endl;
+                cout << "\nBooked Succesfully with seat number " << p.seatNumber << " " << p.alloted << endl;
             }
             else if (p.berthPreferance == "middle")
             {
-               
+
                 p.alloted = p.berthPreferance;
                 ticket.middleBerths--;
                 p.seatNumber = ticket.middlePositions[0];
                 ticket.middlePositions.erase(ticket.middlePositions.begin());
                 ticket.confirmedTickets[p.passengerId] = p;
-                cout << "\nBooked Succesfully with seat number "<<p.seatNumber<<" "<<p.alloted<<endl;
+                cout << "\nBooked Succesfully with seat number " << p.seatNumber << " " << p.alloted << endl;
             }
-            else if(p.berthPreferance == "upper")
+            else if (p.berthPreferance == "upper")
             {
-        
+
                 p.alloted = p.berthPreferance;
                 ticket.upperBerths--;
                 p.seatNumber = ticket.upperPositions[0];
                 ticket.upperPositions.erase(ticket.upperPositions.begin());
                 ticket.confirmedTickets[p.passengerId] = p;
-                cout << "\nBooked Succesfully with seat number "<<p.seatNumber<<" "<<p.alloted<<endl;
+                cout << "\nBooked Succesfully with seat number " << p.seatNumber << " " << p.alloted << endl;
             }
         }
-        else if(ticket.lowerBerths > 0)
+        else if (ticket.lowerBerths > 0)
         {
             cout << "\nPreferred Berth Not Available";
             p.alloted = "lower";
@@ -242,9 +252,9 @@ public:
             p.seatNumber = ticket.lowerPositions[0];
             ticket.lowerPositions.erase(ticket.lowerPositions.begin());
             ticket.confirmedTickets[p.passengerId] = p;
-            cout << "\nBooked Succesfully with seat number "<<p.seatNumber<<" "<<p.alloted<<endl;
+            cout << "\nBooked Succesfully with seat number " << p.seatNumber << " " << p.alloted << endl;
         }
-        else if(ticket.middleBerths > 0)
+        else if (ticket.middleBerths > 0)
         {
             cout << "\nPreferred Berth Not Available";
             p.alloted = "middle";
@@ -252,9 +262,9 @@ public:
             p.seatNumber = ticket.middlePositions[0];
             ticket.middlePositions.erase(ticket.middlePositions.begin());
             ticket.confirmedTickets[p.passengerId] = p;
-            cout << "\nBooked Succesfully with seat number "<<p.seatNumber<<" "<<p.alloted<<endl;
+            cout << "\nBooked Succesfully with seat number " << p.seatNumber << " " << p.alloted << endl;
         }
-        else if(ticket.upperBerths > 0)
+        else if (ticket.upperBerths > 0)
         {
             cout << "\nPreferred Berth Not Available";
             p.alloted = "upper";
@@ -262,68 +272,68 @@ public:
             p.seatNumber = ticket.upperPositions[0];
             ticket.upperPositions.erase(ticket.upperPositions.begin());
             ticket.confirmedTickets[p.passengerId] = p;
-            cout << "\nBooked Succesfully with seat number "<<p.seatNumber<<" "<<p.alloted<<endl;
+            cout << "\nBooked Succesfully with seat number " << p.seatNumber << " " << p.alloted << endl;
         }
-        else if(ticket.rac > 0)
+        else if (ticket.rac > 0)
         {
-            cout<<"\nNo Berths Available...\n";
-            cout<<"1. Book RAC\n";
-            cout<<"2. Go Back\n";
+            cout << "\nNo Berths Available...\n";
+            cout << "1. Book RAC\n";
+            cout << "2. Go Back\n";
             int ch;
-            cout<<"Enter your choice:";
-            cin>>ch;
+            cout << "Enter your choice:";
+            cin >> ch;
             switch (ch)
             {
-                case 1:
-                {
-                    p.alloted = "RAC";
-                    ticket.rac--;
-                    p.seatNumber = ticket.racPositions[0];
-                    ticket.racPositions.erase(ticket.racPositions.begin());
-                    ticket.confirmedTickets[p.passengerId] = p;
-                    ticket.racPassengers.push(p.passengerId);
-                    cout << "\nBooked RAC with seat number "<<p.seatNumber<<" "<<p.alloted<<endl;
-                }
-                break; 
-                case 2:
+            case 1:
+            {
+                p.alloted = "RAC";
+                ticket.rac--;
+                p.seatNumber = ticket.racPositions[0];
+                ticket.racPositions.erase(ticket.racPositions.begin());
+                ticket.confirmedTickets[p.passengerId] = p;
+                ticket.racPassengers.push(p.passengerId);
+                cout << "\nBooked RAC with seat number " << p.seatNumber << " " << p.alloted << endl;
+            }
+            break;
+            case 2:
                 run();
                 break;
-                default:
+            default:
                 run();
             }
         }
-        else if(ticket.waitingList > 0)
+        else if (ticket.waitingList > 0)
         {
-            cout<<"\nNo Berths and RAC Available...\n";
-            cout<<"1. Book in Waiting List\n";
-            cout<<"2. Go Back\n";
+            cout << "\nNo Berths and RAC Available...\n";
+            cout << "1. Book in Waiting List\n";
+            cout << "2. Go Back\n";
             int ch;
-            cout<<"Enter your choice:";
-            cin>>ch;
+            cout << "Enter your choice:";
+            cin >> ch;
             switch (ch)
             {
-                case 1:
-                {
-                    p.alloted = "Waiting List";
-                    ticket.waitingList--;
-                    p.seatNumber = ticket.waitingListPositions[0];
-                    ticket.waitingListPositions.erase(ticket.waitingListPositions.begin());
-                    ticket.confirmedTickets[p.passengerId] = p;
-                    ticket.waitingListPassengers.push(p.passengerId);
-                    cout << "\nBooked in waiting List "<<p.seatNumber<<" "<<p.alloted<<endl;
-                }
-                break; 
-                case 2:
+            case 1:
+            {
+                p.alloted = "Waiting List";
+                ticket.waitingList--;
+                p.seatNumber = ticket.waitingListPositions[0];
+                ticket.waitingListPositions.erase(ticket.waitingListPositions.begin());
+                ticket.confirmedTickets[p.passengerId] = p;
+                ticket.waitingListPassengers.push(p.passengerId);
+                cout << "\nBooked in waiting List " << p.seatNumber << " " << p.alloted << endl;
+            }
+            break;
+            case 2:
                 run();
                 break;
-                default:
+            default:
                 run();
             }
         }
         else
         {
-            cout<<"\nNo Tickets Available!";
-        }    
+            cout << "\nNo Tickets Available!";
+        }
     }
     void run()
     {
@@ -378,6 +388,8 @@ public:
             case 5:
                 exit(0);
                 break;
+            default:
+                run();
             }
         }
     }
